2a8a9607241c579971610834474a5a410d8ea156
Martijn <oldmartijntje@gmail.com>
2025-04-14 22:10:13 +0200

    Extra trim materials

--- src/main/java/com/gameinggrounds/sandbox/GameingGroundsSandboxDataGenerator.java
+++ src/main/java/com/gameinggrounds/sandbox/GameingGroundsSandboxDataGenerator.java
@@ -2,6 +2,7 @@ package com.gameinggrounds.sandbox;
 
 import com.gameinggrounds.sandbox.datagen.*;
 import com.gameinggrounds.sandbox.enchantment.ModEnchantments;
+import com.gameinggrounds.sandbox.trim.ModTrimMaterials;
 import com.gameinggrounds.sandbox.trim.ModTrimPatterns;
 import com.gameinggrounds.sandbox.world.ModConfiguredFeatures;
 import com.gameinggrounds.sandbox.world.ModPlacedFeatures;
@@ -29,5 +30,6 @@ public class GameingGroundsSandboxDataGenerator implements DataGeneratorEntrypoi
 		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
 		registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
 		registryBuilder.addRegistry(RegistryKeys.TRIM_PATTERN, ModTrimPatterns::bootstrap);
+		registryBuilder.addRegistry(RegistryKeys.TRIM_MATERIAL, ModTrimMaterials::bootstrap);
 	}
 }
